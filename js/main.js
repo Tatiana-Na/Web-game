c81b4ccfc2155d9ea3c002b15a683758c5383327
Tatiana-Na <nazarovades@gmail.com>
2022-07-06 10:28:59 +0200

    Work numbers

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 'use strict'
+
+const btn = document.querySelector('.js-btn');
+
+
+const guesses = 0;
+
+function getRandomNumber(max) { 
+    return Math.ceil(Math.random() * max); 
+  }
+
+  const randomNumber = getRandomNumber(100);
+  console.log(randomNumber);
+  
+
+
+
+/* const prompt = document.querySelector('.js-form');
+prompt.addEventListener('submit', handleSubmit);
+ function handleSubmit(event) {
+    event.preventDefault();
+    console.log('imput.value');
+    imput.value = '';
+ }
+ */
+
+btn.addEventListener('click', evt => {
+    evt.preventDefault()
+});
+    console.log('click');
+
+
